52f77c2421090cdea1ea4b01b2788c7d3c0a3bf4
Clinton <njiruclinton56@gmail.com>
2022-08-20 21:19:38 +0300

    favicon

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ import {
 import Contact from './Components/Contact/Contact';
 import About from './Components/About/About';
 import Navbarf from './Components/Navbar/Navbarf';
+import Favicon from 'react-favicon';
+import f1 from "./favicon_io/favicon.png"
+import f2 from "./favicon_io/android.png"
+import f3 from "./favicon_io/apple.png"
 
 function App() {
   return (
     <div className="App">
+      <Favicon url={[f1, f2, f3]} />
       <Router>
         <Navbarf />
         <Routes>
